5408a3f349f2f90473a8daea4f7b6535a1136b76
UO257192 <UO257192@uniovi.es>
2020-05-02 23:17:35 +0200

    Group class test

---
+++ src/utils/group/Group.test.js
@@ -0,0 +1,17 @@
+import {cleanup} from 'react-testing-library';
+import Group from "./Group";
+
+afterAll(cleanup);
+
+describe('Route class domain', () => {
+    it('class create correctly', () => {
+        const group = new Group("Prueba", "Prueba", ["Prueba"]);
+        group.name = "Prueba";
+        group.description = "Prueba";
+        group.participants = ["group"];
+        expect(group.name === "Prueba");
+        expect(group.description === "Prueba");
+        expect(group.participants.length > 0 );
+    });
+
+});
